c807a32c32819ccce2905108420e1282cf648234
aaronliang12 <aliang12@live.unc.edu>
2022-04-24 13:37:52 -0400

    Created 5 variables for Chart.js

--- frontend/main.js
+++ frontend/main.js
@@ -27,14 +27,24 @@ getInformation = async (e) => {
     
     console.log(filteredInformation);
 
+    let totalPopulation = filteredInformation[1724].population; 
     let totalCases = filteredInformation[1724].actuals.cases;
     let totalVaccinated = filteredInformation[1724].actuals.vaccinationsCompleted;
     let totalDeaths = filteredInformation[1724].actuals.deaths;
     let lastUpdateDate = filteredInformation[1724].lastUpdatedDate;
 
+    //5 variables for the Bar Chart 
+    let bedsWithCovidPatientsRatio = filteredInformation[1724].metrics.bedsWithCovidPatientsRatio; 
+    let ICUCapacityRatio = filteredInformation[1724].metrics.icuCapacityRatio; 
+    let infectionRate = filteredInformation[1724].metrics.infectionRate; 
+    let testPositiveRatio = filteredInformation[1724].metrics.testPositivityRatio; 
+    let vacToPopRatio = totalVaccinated / totalPopulation; 
+
+
     console.log(totalCases);
     console.log(totalVaccinated);
 
+    
     let lastUpdateString = "Last Updated: " + lastUpdateDate
     document.getElementById("lastUpdateDate").innerHTML = lastUpdateString
     document.getElementById("totalCases").innerHTML = totalCases
